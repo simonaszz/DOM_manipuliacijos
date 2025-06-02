c7e3b15eca9f6b3a1ec5434f28351ed521dc4735
Simonas Ubartas <simasak56@gmail.com>
2025-06-02 18:59:33 +0300

    add secound section an js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// pirmas div
 document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos anrastes tekstas';
 document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
+
+// antras div
+console.log('ANTRAS DIV');
+let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
+console.log('paimtas su innerHTML tekstas:', tekstasIsParagrafo);
+let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerText;
+console.log('paimtas su innerText tekstas:', tekstasIsParagrafo2);
