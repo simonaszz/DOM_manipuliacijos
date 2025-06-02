014ff641af1e21e0bf85861c1d481915705a964c
Simonas Ubartas <simasak56@gmail.com>
2025-06-02 20:36:20 +0300

    Fifteenth Div - Longest Word

--- index.js
+++ index.js
@@ -1,92 +1,133 @@
-// pirmas div
-document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos anrastes tekstas';
-document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
+// first div
+document.getElementById('first-title').innerHTML = 'New first title text';
+document.getElementById('first-paragraph').innerHTML = 'New changed first paragraph text';
 
-// antras div
-console.log('ANTRAS DIV');
-let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
-console.log('paimtas su innerHTML tekstas:', tekstasIsParagrafo);
-let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerText;
-console.log('paimtas su innerText tekstas:', tekstasIsParagrafo2);
+// second div
+console.log('SECOND DIV');
+let textFromParagraph = document.getElementById('paragraph-text').innerHTML;
+console.log('taken with innerHTML text:', textFromParagraph);
+let textFromParagraph2 = document.getElementById('paragraph-text').innerText;
+console.log('taken with innerText text:', textFromParagraph2);
 
-// trecias div
-console.log('TRECIAS DIV');
-console.log('innerHTML turinys:', document.getElementById('trecias').innerHTML);
-console.log('innerText turinys:', document.getElementById('trecias').innerText);
+// third div
+console.log('THIRD DIV');
+console.log('innerHTML content:', document.getElementById('third').innerHTML);
+console.log('innerText content:', document.getElementById('third').innerText);
 
-// septintas div
-function pakeisti() {
-    document.getElementById('septinto-tekstas').innerHTML = "naujas tekstas";
+// seventh div
+function changeText() {
+    document.getElementById('seventh-text').innerHTML = "new text";
 }
-// astuntas div
-function pakeisti2(elementas, tekstas) {
-    document.getElementById(elementas).innerHTML = tekstas;
-}
-
-// devintas div
-function kitaSpalva(elementas, spalva) {
-    document.getElementById(elementas).style.color = spalva;
-}
-// desimtas div
-var pirmas = 8;
-var antras = 9;
-
-var suma = pirmas + antras;
-var skirtumas = pirmas - antras;
-var sandauga = pirmas * antras;
-var dalmuo = pirmas / antras;
-
-document.getElementById("suma").innerHTML = pirmas + " + " + antras + " = " + suma;
-document.getElementById("skirtumas").innerHTML = pirmas + " - " + antras + " = " + skirtumas;
-document.getElementById("sandauga").innerHTML = pirmas + " * " + antras + " = " + sandauga;
-document.getElementById("dalmuo").innerHTML = pirmas + " / " + antras + " = " + dalmuo;
-
-// vienuoliktas div
-var pirmas = 7;
-var antras = 5;
-
-let isvedimui = `<p>${pirmas} + ${antras} = ${pirmas + antras}</p>
-               <p>${pirmas} - ${antras} = ${pirmas - antras}</p>
-               <p>${pirmas} * ${antras} = ${pirmas * antras}</p>
-               <p>${pirmas} / ${antras} = ${pirmas / antras}</p>`;
-
-document.getElementById('vienuolikto-turinys').innerHTML = isvedimui;
-
-// dvyliktas div
-function keistiSpalva(elementas, spalva) {
-    document.getElementById(elementas).style.color = spalva;
+// eighth div
+function changeText2(element, text) {
+    document.getElementById(element).innerHTML = text;
 }
 
-function keistiDydi(elementas, dydis) {
-    document.getElementById(elementas).style.fontSize = dydis + 'px';
+// ninth div
+function changeColor(element, color) {
+    document.getElementById(element).style.color = color;
+}
+// tenth div
+var first = 8;
+var second = 9;
+
+var sum = first + second;
+var difference = first - second;
+var product = first * second;
+var quotient = first / second;
+
+document.getElementById("sum").innerHTML = first + " + " + second + " = " + sum;
+document.getElementById("difference").innerHTML = first + " - " + second + " = " + difference;
+document.getElementById("product").innerHTML = first + " * " + second + " = " + product;
+document.getElementById("quotient").innerHTML = first + " / " + second + " = " + quotient;
+
+// eleventh div
+var first = 7;
+var second = 5;
+
+let output = `<p>${first} + ${second} = ${first + second}</p>
+               <p>${first} - ${second} = ${first - second}</p>
+               <p>${first} * ${second} = ${first * second}</p>
+               <p>${first} / ${second} = ${first / second}</p>`;
+
+document.getElementById('eleventh-content').innerHTML = output;
+
+// twelfth div
+function setColor(element, color) {
+    document.getElementById(element).style.color = color;
+}
+
+function setFontSize(element, size) {
+    document.getElementById(element).style.fontSize = size + 'px';
 }
 
 
-// tryliktas div
-let divKeitimui = document.getElementById('div-keitimui');
+// thirteenth div
+let divToChange = document.getElementById('div-to-change');
 
-let plocioSlider = document.getElementById('plocio-slider');
-plocioSlider.oninput = function () {
-    divKeitimui.style.width = this.value + 'px';
+let widthSlider = document.getElementById('width-slider');
+widthSlider.oninput = function () {
+    divToChange.style.width = this.value + 'px';
 }
 
-let auksioSlider = document.getElementById('auksio-slider');
-auksioSlider.oninput = function () {
-    divKeitimui.style.height = this.value + 'px';
+let heightSlider = document.getElementById('height-slider');
+heightSlider.oninput = function () {
+    divToChange.style.height = this.value + 'px';
 }
-let spalvosPasirinkimas = document.getElementById('spalvos-pasirinkimas');
-spalvosPasirinkimas.oninput = function () {
-    divKeitimui.style.backgroundColor = this.value;
+let colorPicker = document.getElementById('color-picker');
+colorPicker.oninput = function () {
+    divToChange.style.backgroundColor = this.value;
 }
-let tekstasDivui = document.getElementById('tekstas-divui');
-tekstasDivui.oninput = function () {
-    divKeitimui.innerHTML = this.value;
+let textForDiv = document.getElementById('text-for-div');
+textForDiv.oninput = function () {
+    divToChange.innerHTML = this.value;
 }
 
-// keturioliktas div
-let tekstasDivui = document.getElementById('tekstas-divui'); // This variable is reused from the previous div. It should be unique or passed. Let's define it locally or ensure the ID is unique.
-// Correction: `tekstasDivui` was used for the input, `zodziu-inputas` is the textarea here.
+// fourteenth div
+// Correction: `textForDiv` was used for the input, `words-input` is the textarea here.
 
-function pridetiZodi(elementas, zodis) {
-    document.getElementById(elementas).value += zodis + " "; // Use .value for textarea
+function addWord(element, word) {
+    document.getElementById(element).value += word + " "; // Use .value for textarea
+}
+// fifteenth div
+function wordCount() {
+    let text = document.getElementById('fifteenth-text').value;
+    let words = text.trim().split(' '); // .trim() to remove leading/trailing spaces, .split() to create array
+    let wordCount = words.filter(word => word.length > 0).length; // Filter out empty strings from multiple spaces
+
+    document.getElementById('fifteenth-answers').innerHTML = `<p>Word count: ${wordCount}</p>`;
+}
+function longestWord() {
+    let text = document.getElementById('fifteenth-text').value;
+    let words = text.trim().split(' ');
+    let longest = '';
+
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+    document.getElementById('fifteenth-answers').innerHTML += `<p>Longest word: ${longest}</p>
+    <p>Its length: ${longest.length} characters.</p>`;
+}
+// penkioliktas div
+function zodziuKiekis() {
+    let tekstas = document.getElementById('penkiolikto-tekstas').value;
+    let zodziai = tekstas.trim().split(' '); // .trim() to remove leading/trailing spaces, .split() to create array
+    let zodziuKiekis = zodziai.filter(word => word.length > 0).length; // Filter out empty strings from multiple spaces
+
+    document.getElementById('penkiolikto-atsakymai').innerHTML = `<p>Žodžių kiekis: ${zodziuKiekis}</p>`;
+}
+function ilgiausiasZodis() {
+    let tekstas = document.getElementById('penkiolikto-tekstas').value;
+    let zodziai = tekstas.trim().split(' ');
+    let ilgiausias = '';
+
+    for (let zodis of zodziai) {
+        if (zodis.length > ilgiausias.length) {
+            ilgiausias = zodis;
+        }
+    }
+    document.getElementById('penkiolikto-atsakymai').innerHTML += `<p>Ilgiausias žodis: ${ilgiausias}</p>
+    <p>Jo ilgis: ${ilgiausias.length} simbolių.</p>`;
 }
