612b94af372c9e61cbc67a56fa54c0c1793f77b8
Simonas Ubartas <simasak56@gmail.com>
2025-06-02 19:01:18 +0300

    add 3 div and js

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
 console.log('paimtas su innerHTML tekstas:', tekstasIsParagrafo);
 let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerText;
 console.log('paimtas su innerText tekstas:', tekstasIsParagrafo2);
+
+// trecias div
+console.log('TRECIAS DIV');
+console.log('innerHTML turinys:', document.getElementById('trecias').innerHTML);
+console.log('innerText turinys:', document.getElementById('trecias').innerText);
