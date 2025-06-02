461209aaa5036f573416f27e4732d186716c716d
Simonas Ubartas <simasak56@gmail.com>
2025-06-02 18:52:14 +0300

    add css and html, fixed css name

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-// All JavaScript will be added here
+document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos anrastes tekstas';
+document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
